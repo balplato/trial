c0ca324b6104132bcd94dc718370af8e83951665
balplato <balplato@protonmail.ch>
2019-05-13 23:40:35 +0300

    Firefox google-auto-reg

---
+++ google-auto-reg(firefox).py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import time
+login = input("Login:\n")
+password = input("Password:\n")
+# a path should be specified on your own (don't forget that for Windows we use double-backslash \\ ) 
+# however we don't need the path if there is driver in the same directory with .py
+# so, '''fox = webdriver.Firefox(executable_path="C:\\geckodriver.exe")''' is not necessary.
+fox = webdriver.Firefox()
+fox.get('https://www.google.com')
+fox.find_element_by_id('gb_70').click()
+fox.find_element_by_id('identifierId').send_keys(login)
+fox.find_element_by_id('identifierNext').click()
+time.sleep(2)
+# unfortunately, "fox.implicitly_wait(2)" did not work for me
+fox.find_element_by_name('password').send_keys(password)
+fox.find_element_by_id('passwordNext').click()
